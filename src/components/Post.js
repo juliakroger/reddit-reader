4eb1f557e6fc6e908c52efb6ec288cf48346953c
Julia Kroger <julia.kroger@gmail.com>
2020-12-27 14:32:52 -0300

    fix: Post refactor

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,45 +1,56 @@
-import React from 'react';
-import {Card, Image, Icon, List} from 'semantic-ui-react';
-import Moment from 'react-moment';
+import React from "react";
+import { Card, Image, Icon, List } from "semantic-ui-react";
+import Moment from "react-moment";
 
-
-const Post = (props) => {
-  let color = {cursor: 'pointer', color: '#f2711c'};
-  if (props.post.visited) color = {cursor: 'pointer', color: '#a09ba2'};
+const Post = ({ post, deleteThisPost, currentPostHandler, imageWidth }) => {
+  let color = { cursor: "pointer", color: "#f2711c" };
+  if (post.visited) color = { cursor: "pointer", color: "#a09ba2" };
 
   return (
-      <Card style={{width: '100%', minWidth: '250px'}} centered>
-        <Card.Content>
-          <List floated='right' horizontal>
-            <List.Item>
-              <Icon name='remove circle' color='orange' onClick={() => props.deleteThisPost(props.post)} style={{cursor: 'pointer'}}/>
-            </List.Item>
+    <Card style={{ width: "100%", minWidth: "250px" }} centered>
+      <Card.Content>
+        <List floated="right" horizontal>
+          <List.Item>
+            <Icon
+              name="remove circle"
+              color="orange"
+              onClick={() => deleteThisPost(post)}
+              style={{ cursor: "pointer" }}
+            />
+          </List.Item>
+        </List>
+        <List horizontal>
+          <List.Item>
+            <Card.Header onClick={() => currentPostHandler(post)} style={color}>
+              {post.title}
+            </Card.Header>
+            <Card.Meta>Created by {post.author}</Card.Meta>
+          </List.Item>
+        </List>
+      </Card.Content>
+      <Card.Content>
+        {post.post_hint === "image" ? (
+          <Image src={post.url} style={{ width: imageWidth }} />
+        ) : (
+          <a href={post.url}>{post.url}</a>
+        )}
+      </Card.Content>
 
-          </List>
-          <List horizontal>
-            <List.Item>
-              <Card.Header onClick={() => props.currentPostHandler(props.post)} style={color}>{props.post.title}</Card.Header>
-              <Card.Meta>Created by {props.post.author}</Card.Meta>
-            </List.Item>
-          </List>
-        </Card.Content>
-        <Card.Content>
-          {
-            (props.post.post_hint === 'image') ? <Image src={props.post.url} style={{width: props.imageWidth}}/> :
-                <a href={props.post.url}>{props.post.url}</a>
-          }
-        </Card.Content>
-
-        <Card.Content>
-          <List floated='right' horizontal>
-            <List.Item>
-              <Icon name='comment outline' color='orange'/> {props.post.num_comments} Comments
-            </List.Item>
-          </List>
-          <List horizontal><List.Item><Moment fromNow>{props.post.created*1000}</Moment></List.Item></List>
-        </Card.Content>
-      </Card>
+      <Card.Content>
+        <List floated="right" horizontal>
+          <List.Item>
+            <Icon name="comment outline" color="orange" /> {post.num_comments}{" "}
+            Comments
+          </List.Item>
+        </List>
+        <List horizontal>
+          <List.Item>
+            <Moment fromNow>{post.created * 1000}</Moment>
+          </List.Item>
+        </List>
+      </Card.Content>
+    </Card>
   );
 };
 
-  export default Post;
+export default Post;
