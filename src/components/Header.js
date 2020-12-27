ab005d712e8c5ccb0a1aa9258c97d0d0ce749386
Julia Kroger <julia.kroger@gmail.com>
2020-12-27 14:32:10 -0300

    fix: Header refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import { Menu, Segment, Button } from 'semantic-ui-react'
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Menu, Segment, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-class Header extends Component {
-  render() {
-    return (
-        <Segment inverted style={{margin: '-5px'}}>
-          <Menu inverted>
-            <Link to="/"><Button secondary>All posts</Button></Link>
-            <Link to="/saved"><Button secondary>My saved posts</Button></Link>
-          </Menu>
-        </Segment>
-    );
-  };
+const Header = () => {
+  return (
+    <Segment inverted style={{ margin: "-5px" }}>
+      <Menu inverted>
+        <Link to="/">
+          <Button secondary>All posts</Button>
+        </Link>
+        <Link to="/saved">
+          <Button secondary>My saved posts</Button>
+        </Link>
+      </Menu>
+    </Segment>
+  );
 };
 
 export default Header;
