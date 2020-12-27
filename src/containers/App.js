be3faafc9f6c33b06363af10bdc16a1cd33184d1
Julia Kroger <julia.kroger@gmail.com>
2020-12-27 14:33:02 -0300

    fix: App refactor

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
+import React, { useEffect } from "react";
 import axios from "axios";
-import {BrowserRouter, Route} from "react-router-dom";
-import Header from '../components/Header';
-import PostsPage from './PostsPage';
-import MyPosts from './MyPosts';
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "../components/Header";
+import PostsPage from "./PostsPage";
+import MyPosts from "./MyPosts";
 
-class App extends Component {
-  componentWillMount()  {
-    axios.get('https://www.reddit.com/r/rickandmorty/top.json?limit=50')
-        .then(res => {
-          localStorage.setItem('data', JSON.stringify(res.data.data));
-        })
-        .catch(error => console.log(error))
-  };
+const App = () => {
+  useEffect(() => {
+    axios
+      .get("https://www.reddit.com/r/rickandmorty/top.json?limit=50")
+      .then((res) => {
+        localStorage.setItem("data", JSON.stringify(res.data.data));
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <Header/>
-          <Route exact path='/' component={PostsPage} />
-          <Route path='/saved' component={MyPosts} />
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={PostsPage} />
+        <Route path="/saved" component={MyPosts} />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
